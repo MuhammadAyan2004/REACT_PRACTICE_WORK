2e1c56e8646b1360ac3482f7343238ca4ad5dfcd
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-06 05:21:59 +0500

    Create App.jsx

---
+++ class_1/src/App.jsx
@@ -0,0 +1,18 @@
+import DemoButton from './demoButton.jsx'
+import {Greetings} from './greetings'
+import {Random} from './Random'
+
+
+function App (){
+  return <div>
+    <h1>
+      i am learning react 
+    </h1>
+    <Greetings></Greetings>
+    <DemoButton></DemoButton>
+    <Random></Random>
+  </div>
+  
+}
+
+export default App
