c96c3185a11e3214e0a48da9f64d8df6d38c1b05
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-07 05:17:22 +0500

    components

---
+++ projects/src/component/todoitem1.jsx
@@ -0,0 +1,19 @@
+function TodoItem1() {
+  let todoName = "meeting with Tech solution company";
+  let todoDate = "7/7/2026";
+  return (
+    <div class="container text-left">
+      <div class="row">
+        <div class="col-6">{todoName}</div>
+        <div class="col-4">{todoDate}</div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem1;
