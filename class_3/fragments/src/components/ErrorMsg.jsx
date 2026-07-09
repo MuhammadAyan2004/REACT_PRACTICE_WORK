cd61a5872c49c408139bb4cdfad2887483b0fd52
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-09 23:31:27 +0500

    Update ErrorMsg.jsx

--- class_3/fragments/src/components/ErrorMsg.jsx
+++ class_3/fragments/src/components/ErrorMsg.jsx
@@ -1,7 +1,7 @@
 function ErrorMsg({ items }) {
   return (
     <>
-      {items.length === 0 && <h1>There is no foods items to be displayed</h1>}
+      {items.length === 0 && <h1>There is no foods items to be displayed in this field</h1>}
     </>
   );
 }
