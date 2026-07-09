cfe636023cedd8fcefce643fc643d268fbfe762e
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-10 04:00:50 +0500

    Create TodoTitle.jsx

---
+++ projects/3_todo_App_version_two/src/component/TodoTitle.jsx
@@ -0,0 +1,11 @@
+import styles from "./TodoTitle.module.css"
+
+function TodoName() {
+  return (
+    <div>
+      <h1 className={styles.todoHeading}>Todo App</h1>
+    </div>
+  );
+}
+
+export default TodoName;
