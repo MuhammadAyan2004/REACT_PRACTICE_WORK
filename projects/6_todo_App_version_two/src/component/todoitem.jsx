d65fea97bdf8376cb205b0104bbeb5a3f3715c3c
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-15 03:12:15 +0500

    Update todoitem.jsx

--- projects/6_todo_App_version_two/src/component/todoitem.jsx
+++ projects/6_todo_App_version_two/src/component/todoitem.jsx
@@ -1,12 +1,27 @@
-function TodoItem({ todoName, todoDate }) {
+import { MdDelete } from "react-icons/md";
+import { CiEdit } from "react-icons/ci";
+
+function TodoItem({ todoName, todoDate, onClickDelete, onClickEdit, index }) {
   return (
     <div className="container text">
       <div className="row">
         <div className="col-6">{todoName}</div>
-        <div className="col-4">{todoDate}</div>
-        <div className="col-2">
-          <button type="button" className="btn btn-danger button">
-            Delete
+        <div className="col-3">{todoDate}</div>
+        <div className="col-3 btnSec">
+          <button
+            type="button"
+            className="btn btn-success button"
+            onClick={() => onClickEdit(index)}
+          >
+            <CiEdit />
+          </button>
+
+          <button
+            type="button"
+            className="btn btn-danger button"
+            onClick={() => onClickDelete(index)}
+          >
+            <MdDelete />
           </button>
         </div>
       </div>
