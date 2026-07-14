d0c10107d1b77b300e994857d63f615eacf9677b
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-15 03:12:12 +0500

    Update addTodo.jsx

--- projects/6_todo_App_version_two/src/component/addTodo.jsx
+++ projects/6_todo_App_version_two/src/component/addTodo.jsx
@@ -1,24 +1,34 @@
-function AddTodo({ addTodo, todoName, todoDate }) {
+// import { useState } from "react";
+import { IoMdAdd } from "react-icons/io";
+
+function AddTodo({
+  addTodo,
+  todoName,
+  todoDate,
+  handleTodoName,
+  handleTodoDate,
+}) {
   return (
-    <div className="container ">
+    <div className="container inputContainer">
       <div className="row text">
         <div className="col-6">
           <input
             type="text"
             placeholder="Enter your Todo"
-            onChange={(e)=>todoName(e)}
+            value={todoName}
+            onChange={(e) => handleTodoName(e.target.value)}
           />
         </div>
         <div className="col-4">
-          <input type="date" onChange={(e)=>todoDate(e)} />
+          <input type="date" onChange={(e)=>handleTodoDate(e.target.value)} value={todoDate} />
         </div>
         <div className="col-2 ">
           <button
             type="button"
             className="btn btn-success button"
-            onClick={addTodo}
+            onClick={() => addTodo(todoName, todoDate)}
           >
-            ADD
+            <IoMdAdd />
           </button>
         </div>
       </div>
