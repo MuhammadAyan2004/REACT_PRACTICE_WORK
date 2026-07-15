a8162176a88a2c726f645140f19a46aa906e4383
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-16 04:47:47 +0500

    Create App.jsx

---
+++ class_4_useRef/6_todo_App_version_two/src/App.jsx
@@ -0,0 +1,77 @@
+import TodoName from "./component/TodoTitle";
+import AddTodo from "./component/addTodo";
+import TodoItems from "./component/TodoItems";
+import "./Apps.css";
+import { useEffect, useState} from "react";
+
+function App() {
+
+  const [todoItems, setTodoItems] = useState(() => {
+    const data = localStorage.getItem("todo-list");
+    return data ? JSON.parse(data) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todo-list", JSON.stringify(todoItems));
+  }, [todoItems]);
+
+  const [todoName, setTodoName] = useState("");
+  const [date, setDate] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
+
+
+  const clickToAdd = (todoName, date) => {
+    if (todoName.trim() === "" || date === "") {
+      alert("please enter values in the required field");
+      return;
+    }
+    if (editIndex !== null) {
+      const updatedTodos = [...todoItems];
+      updatedTodos[editIndex] = { name: todoName, date: date };
+      setTodoItems(updatedTodos);
+      setEditIndex(null);
+      clearInputs();
+    } else {
+      let newTodo = [...todoItems, { name: todoName, date: date }];
+      setTodoItems(newTodo);
+      clearInputs();
+    }
+  };
+
+  const handleDeleteButton = (index) => {
+    const newTodo = todoItems.filter((_, i) => i !== index);
+    setTodoItems(newTodo);
+  };
+  const handleEditButton = (index) => {
+    setTodoName(todoItems[index].name);
+    setDate(todoItems[index].date);
+    setEditIndex(index);
+  };
+
+  function clearInputs() {
+    setTodoName("");
+    setDate("");
+  }
+
+  return (
+    <center>
+      <div className="main">
+        <TodoName />
+        <AddTodo
+          addTodo={clickToAdd}
+          todoName={todoName}
+          todoDate={date}
+          handleTodoName={setTodoName}
+          handleTodoDate={setDate}
+        />
+        <TodoItems
+          todos={todoItems}
+          onClickDelete={handleDeleteButton}
+          onClickEdit={handleEditButton}
+        />
+      </div>
+    </center>
+  );
+}
+
+export default App;
