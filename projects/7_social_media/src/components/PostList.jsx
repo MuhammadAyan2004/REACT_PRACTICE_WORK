a999df90d8408170ba00d2b1b260258fe80ed900
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-19 03:53:39 +0500

    components

---
+++ projects/7_social_media/src/components/PostList.jsx
@@ -0,0 +1,13 @@
+import Post from "./post";
+
+const PostList = ()=>{
+    return (
+      <>
+        <Post></Post>
+        <Post></Post>
+        <Post></Post>
+      </>
+    );
+}
+
+export default PostList
