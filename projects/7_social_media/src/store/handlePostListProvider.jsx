5652a81c3f285190c551c7a892a1fc121f0dd6f3
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-21 02:29:49 +0500

    Update handlePostListProvider.jsx

--- projects/7_social_media/src/store/handlePostListProvider.jsx
+++ projects/7_social_media/src/store/handlePostListProvider.jsx
@@ -1,54 +1,105 @@
-import { useReducer } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { PostList_provider } from "./postList_context";
 
+
 const postListReducer = (currPostList, action) => {
-    let updatedList;
-    if (action.type === "delete_post"){
-        updatedList = currPostList.filter(card => card.id !== action.payload.postId)
+    
+  let updatedList;
+  if (action.type === "Add_post") {
+    updatedList = [
+      {
+        id: action.payload.postId,
+        userId: action.payload.postUserId,
+        title: action.payload.postTitle,
+        body: action.payload.postDescription,
+        tags: action.payload.postHashtag,
+        reactions: 0,
+      },
+      ...currPostList,
+    ];
+  } else if (action.type === "delete_post") {
+    updatedList = currPostList.filter(
+      (card) => card.id !== action.payload.postId,
+    );
+  } else if (action.type === "new_like") {
+    console.log(action.payload.initialUser);
+    if (action.payload.initialUser.includes(action.payload.userId)) {
+      updatedList = currPostList.map((post) => {
+        return post.id === action.payload.postId
+          ? { ...post, reactions: post.reactions - 1 }
+          : post;
+      });
+      action.payload.setUser("");
+    } else {
+      updatedList = currPostList.map((post) => {
+        return post.id === action.payload.postId
+          ? { ...post, reactions: post.reactions + 1 }
+          : post;
+      });
+      action.payload.setUser(action.payload.userId);
     }
-    return updatedList;
+  }
+  return updatedList;
 };
 
 const HandlePosts = ({ children }) => {
+  const fetchData = () => {
+    const data = localStorage.getItem("postList_data");
+    return data ? JSON.parse(data) : [];
+  };
   const [postList, dispatchedPostList] = useReducer(
     postListReducer,
-    default_Values,
+    [],
+    fetchData,
   );
-  const addPost = (postTitle, postDescription, postHashtag) => {
-    console.log(`${postTitle}, ${postDescription}, ${postHashtag}`);
+
+  useEffect(() => {
+    localStorage.setItem("postList_data", JSON.stringify(postList));
+  }, [postList]);
+
+  const [initialUser, setUser] = useState([]);
+
+  const addPost = (
+    postId,
+    postUserId,
+    postTitle,
+    postDescription,
+    postHashtag,
+  ) => {
+    dispatchedPostList({
+      type: "Add_post",
+      payload: {
+        postId,
+        postUserId,
+        postTitle,
+        postDescription,
+        postHashtag,
+      },
+    });
   };
   const deletePost = (postId) => {
     const deletedPost = {
-        type:"delete_post",
-        payload :{postId}
-    }
-    dispatchedPostList(deletedPost)
+      type: "delete_post",
+      payload: { postId },
+    };
+    dispatchedPostList(deletedPost);
+  };
+
+  const handleLike = (postId, userId) => {
+    // console.log(postId);
+    dispatchedPostList({
+      type: "new_like",
+      payload: { postId, userId, initialUser ,setUser },
+    });
   };
 
   return (
-    <PostList_provider.Provider value={{postList, addPost, deletePost}}>
+    <PostList_provider.Provider
+      value={{ postList, addPost, deletePost, handleLike }}
+    >
       {children}
     </PostList_provider.Provider>
   );
 };
-
-const default_Values = [
-  {
-    id: 1,
-    title: "feeling happy today",
-    body: "I am learning react dev and making improvement day by day. eventually i'm very excited to share this great news with all firends and family members",
-    tags: ["#happy", "#learningReact", "#goodDay"],
-    reactions: 5,
-    userId: "user123",
-  },
-  {
-    id: 2,
-    title: "Imran khan become the prime minister of Pakistan. In Sh Allah",
-    body: "The genz of Pakistan are forcing to the goverment and laws to realeased Imran khan and doing election again.",
-    tags: ["#genz", "#freeImran khan", "#revolution"],
-    reactions: 9,
-    userId: "user928",
-  },
-];
 
 export default HandlePosts;
