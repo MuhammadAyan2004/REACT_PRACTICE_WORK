b453e8bf094fa11250c7b719f09eea65ba90b04e
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-20 21:06:34 +0500

    Update createPost.jsx

--- projects/7_social_media/src/components/createPost.jsx
+++ projects/7_social_media/src/components/createPost.jsx
@@ -1,40 +1,86 @@
+import { useContext, useRef } from "react";
+import { PostList_provider } from "../store/postList_context";
+
 const CreatePost = () => {
+  const { addPost } = useContext(PostList_provider);
+  let postIdElem = useRef(0)
+  let postUserIdElem = useRef("");
+  let postTitleElem = useRef("")
+  let postDescriptionElem = useRef("");
+  let postHashtagElem = useRef("");
+
+  const submitPost = (e)=>{
+    e.preventDefault()
+    let postId = Number(postIdElem.current.value ++) 
+    let postUserId = postUserIdElem.current.value; 
+    let postTitle = postTitleElem.current.value 
+    let postDescription = postDescriptionElem.current.value; 
+    let postHashtag = postHashtagElem.current.value.split(/(\s+)/);
+
+    addPost(postId, postTitle, postDescription, postHashtag, postUserId);
+  }
   return (
-    <form>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">
-          Email address
-        </label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
-        <div id="emailHelp" class="form-text">
-          We'll never share your email with anyone else.
+    <div className="createPostArea">
+      <form className="innerBox">
+        <strong>
+          <center>Creating post</center>
+        </strong>
+        <input type="text" ref={postIdElem} hidden />
+        <div className="mb-3">
+          <label htmlFor="user_Id" className="form-label">
+            user Id
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="user_Id"
+            ref={postUserIdElem}
+          />
         </div>
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">
-          Password
-        </label>
-        <input
-          type="password"
-          class="form-control"
-          id="exampleInputPassword1"
-        />
-      </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-        <label class="form-check-label" for="exampleCheck1">
-          Check me out
-        </label>
-      </div>
-      <button type="submit" class="btn btn-primary">
-        Submit
-      </button>
-    </form>
+        <div className="mb-3">
+          <label htmlFor="Post_Title" className="form-label">
+            Post Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="Post_Title"
+            ref={postTitleElem}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Description" className="form-label">
+            Description
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="Description"
+            ref={postDescriptionElem}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Add_Hashtags" className="form-label">
+            Add Hashtags
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="Add_Hashtags"
+            ref={postHashtagElem}
+          />
+        </div>
+        <center>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={submitPost}
+          >
+            share post
+          </button>
+        </center>
+      </form>
+    </div>
   );
 };
 
