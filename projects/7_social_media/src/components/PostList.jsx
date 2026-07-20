050ae3b5d84ee72abdad0cbfabb8ca45c14e9f76
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-20 21:06:30 +0500

    Update PostList.jsx

--- projects/7_social_media/src/components/PostList.jsx
+++ projects/7_social_media/src/components/PostList.jsx
@@ -1,13 +1,16 @@
+import { useContext } from "react";
 import Post from "./post";
+import { PostList_provider } from "../store/postList_context";
 
-const PostList = ()=>{
-    return (
-      <>
-        <Post></Post>
-        <Post></Post>
-        <Post></Post>
-      </>
-    );
-}
+const PostList = () => {
+  const {postList} = useContext(PostList_provider);
+  return (
+    <>
+      {postList.map((post) => (
+        <Post key={post.id} post={post}></Post>
+      ))}
+    </>
+  );
+};
 
-export default PostList
+export default PostList;
