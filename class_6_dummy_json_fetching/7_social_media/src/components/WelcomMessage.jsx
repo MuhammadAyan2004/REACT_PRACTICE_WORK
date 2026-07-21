b28079e2df2221297764018f79facc8c47595928
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-22 02:53:29 +0500

    Create WelcomMessage.jsx

---
+++ class_6_dummy_json_fetching/7_social_media/src/components/WelcomMessage.jsx
@@ -0,0 +1,12 @@
+const WelcomMessage = ({showPosts}) => {
+  return (
+    <>
+      <h1>There are no posts</h1>
+      <button type="button" className="btn btn-primary" onClick={showPosts}>
+        Get post from server
+      </button>
+    </>
+  );
+};
+
+export default WelcomMessage;
