3796c7d98c191881184759690e0601c44a91e9cd
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-22 02:53:10 +0500

    Create App.jsx

---
+++ class_6_dummy_json_fetching/7_social_media/src/App.jsx
@@ -0,0 +1,37 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Sidebar from "./components/sidebar";
+import CreatePost from "./components/createPost";
+import PostList from "./components/PostList";
+import { useState } from "react";
+import HandlePosts from "./store/handlePostListProvider";
+
+function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
+
+  return (
+    <HandlePosts>
+      <div className="app-container">
+        <div className="sidebar-section">
+          <Sidebar
+            selectedTab={selectedTab}
+            setSelectedTab={setSelectedTab}
+          ></Sidebar>
+        </div>
+        <div className="content">
+          <Header></Header>
+          {selectedTab === "Home" ? (
+            <PostList />
+          ) : (
+            <CreatePost setSelectedTab={setSelectedTab} />
+          )}
+          <Footer />
+        </div>
+      </div>
+    </HandlePosts>
+  );
+}
+
+export default App;
