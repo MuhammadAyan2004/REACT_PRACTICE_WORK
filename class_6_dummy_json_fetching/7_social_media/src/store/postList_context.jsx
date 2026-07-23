7955d318dc891189188e0bbc63258e6b283d3d5b
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-23 16:44:48 +0500

    Create postList_context.jsx

---
+++ class_6_dummy_json_fetching/7_social_media/src/store/postList_context.jsx
@@ -0,0 +1,11 @@
+import { createContext} from "react";
+
+export const PostList_provider = createContext({
+  postList: [],
+  addPost: () => {},
+  addInitialPosts: () =>{},
+  deletePost: () => {},
+  handleLike: () => {},
+});
+
+
