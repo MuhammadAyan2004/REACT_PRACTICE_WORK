ec74c7bd4ab0f12c74e313e1372c6afb8b7c3c8a
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-23 16:44:38 +0500

    Update WelcomMessage.jsx

--- class_6_dummy_json_fetching/7_social_media/src/components/WelcomMessage.jsx
+++ class_6_dummy_json_fetching/7_social_media/src/components/WelcomMessage.jsx
@@ -1,10 +1,10 @@
-const WelcomMessage = ({showPosts}) => {
+const WelcomMessage = () => {
   return (
     <>
       <h1>There are no posts</h1>
-      <button type="button" className="btn btn-primary" onClick={showPosts}>
+      {/* <button type="button" className="btn btn-primary" onClick={showPosts}>
         Get post from server
-      </button>
+      </button> */}
     </>
   );
 };
