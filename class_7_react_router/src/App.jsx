0848a7a6a335c13f5e4d86877e49f3cd485d23eb
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-27 20:56:16 +0500

    Update App.jsx

--- class_7_react_router/src/App.jsx
+++ class_7_react_router/src/App.jsx
@@ -3,34 +3,33 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Sidebar from "./components/sidebar";
-import CreatePost from "./components/createPost";
-import PostList from "./components/PostList";
+// import CreatePost from "./components/createPost";
+// import PostList from "./components/PostList";
 import { useState } from "react";
 import HandlePosts from "./store/handlePostListProvider";
+import { Outlet} from "react-router-dom";
 
 function App() {
   const [selectedTab, setSelectedTab] = useState("Home");
 
   return (
-    <HandlePosts>
-      <div className="app-container">
-        <div className="sidebar-section">
-          <Sidebar
-            selectedTab={selectedTab}
-            setSelectedTab={setSelectedTab}
-          ></Sidebar>
+    <>
+      <HandlePosts>
+        <div className="app-container">
+          <div className="sidebar-section">
+            <Sidebar
+              selectedTab={selectedTab}
+              setSelectedTab={setSelectedTab}
+            ></Sidebar>
+          </div>
+          <div className="content">
+            <Header></Header>
+            <Outlet />
+            <Footer />
+          </div>
         </div>
-        <div className="content">
-          <Header></Header>
-          {selectedTab === "Home" ? (
-            <PostList />
-          ) : (
-            <CreatePost setSelectedTab={setSelectedTab} />
-          )}
-          <Footer />
-        </div>
-      </div>
-    </HandlePosts>
+      </HandlePosts>
+    </>
   );
 }
 
