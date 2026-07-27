70456fa0169b80ad3ec65dd1333255bd6b15714a
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-27 20:56:23 +0500

    Create initialloader.jsx

---
+++ class_7_react_router/src/components/initialloader.jsx
@@ -0,0 +1,11 @@
+const InitialLoader = ()=>{
+    return (
+      <div className="alignCenter">
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
+}
+
+export default InitialLoader;
