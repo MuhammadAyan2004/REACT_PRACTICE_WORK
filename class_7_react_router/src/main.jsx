7af6ecac684d7c31fd0540ea7752ddd86cbdcd62
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-28 02:50:50 +0500

    Update main.jsx

--- class_7_react_router/src/main.jsx
+++ class_7_react_router/src/main.jsx
@@ -1,9 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CreatePost from "./components/createPost.jsx";
+import PostList from "./components/PostList.jsx";
+import { postLoader } from "./components/postloader.jsx";
+import LoadingFallback from "./components/LoadingFallback.jsx";
+import { createPostAction } from "./components/createPostAction.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    HydrateFallback: LoadingFallback,
+    children: [
+      { path: "/", Component: PostList, loader: postLoader },
+      { path: "/createPost", Component: CreatePost, action:createPostAction },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
