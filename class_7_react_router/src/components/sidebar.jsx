ef0c7a491f50240eb8998bae5b759e6b3def5740
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-28 02:50:48 +0500

    Update sidebar.jsx

--- class_7_react_router/src/components/sidebar.jsx
+++ class_7_react_router/src/components/sidebar.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Sidebar = ({ selectedTab, setSelectedTab }) => {
   return (
     <div
@@ -20,91 +22,33 @@ const Sidebar = ({ selectedTab, setSelectedTab }) => {
         </svg>{" "}
         <span className="fs-4">Posting X</span>{" "}
       </a>{" "}
-      <hr />{" "}
+      <hr />
       <ul className="nav nav-pills flex-column mb-auto">
-        {" "}
         <li className="nav-item" onClick={() => setSelectedTab("Home")}>
-          {" "}
-          <a
-            href="#"
+          <Link
+            to="/"
             className={`nav-link text-white ${selectedTab === "Home" && "active"}`}
             aria-current="page"
           >
-            <svg
-              className="bi pe-none me-2"
-              width="16"
-              height="16"
-              aria-hidden="false"
-            >
-              <use xlinkHref="#home"></use>
-            </svg>
+            <svg className="bi pe-none me-2" width="16" height="16"></svg>
             Home
-          </a>
+          </Link>
         </li>
         <li onClick={() => setSelectedTab("Create Post")}>
-          <a
-            href="#"
+          <Link
+            to="/createPost"
             className={`nav-link text-white ${selectedTab === "Create Post" && "active"}`}
           >
-            {" "}
             <svg
               className="bi pe-none me-2"
               width="16"
               height="16"
               aria-hidden="true"
-            >
-              <use xlinkHref="#speedometer2"></use>
-            </svg>
+            ></svg>
             Create Post
-          </a>{" "}
-        </li>{" "}
-      </ul>{" "}
-      <hr />{" "}
-      <div className="dropdown">
-        {" "}
-        <a
-          href="#"
-          className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          {" "}
-          <img
-            src="https://github.com/mdo.png"
-            alt=""
-            width="32"
-            height="32"
-            className="rounded-circle me-2"
-          />{" "}
-          <strong>settings</strong>{" "}
-        </a>{" "}
-        <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
-          {" "}
-          <li>
-            <a className="dropdown-item" href="#">
-              New project...
-            </a>
-          </li>{" "}
-          <li>
-            <a className="dropdown-item" href="#">
-              Settings
-            </a>
-          </li>{" "}
-          <li>
-            <a className="dropdown-item" href="#">
-              Profile
-            </a>
-          </li>{" "}
-          <li>
-            <hr className="dropdown-divider" />
-          </li>{" "}
-          <li>
-            <a className="dropdown-item" href="#">
-              Sign out
-            </a>
-          </li>{" "}
-        </ul>{" "}
-      </div>{" "}
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
