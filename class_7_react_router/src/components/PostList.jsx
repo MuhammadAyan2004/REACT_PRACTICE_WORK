df355ee8837d8e5ee6306e24924a94985dfe895e
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-28 02:50:45 +0500

    Update PostList.jsx

--- class_7_react_router/src/components/PostList.jsx
+++ class_7_react_router/src/components/PostList.jsx
@@ -1,18 +1,19 @@
-import { useContext } from "react";
 import Post from "./post";
-import { PostList_provider } from "../store/postList_context";
 import WelcomMessage from "./WelcomMessage";
 import Loader from "./loader";
+import { useLoaderData} from "react-router-dom";
+import { useContext } from "react";
+import { PostList_provider } from "../store/postList_context";
 
 const PostList = () => {
-  const { postList, loading } = useContext(PostList_provider);
-
+  const { loading, postList } = useContext(PostList_provider);
+  const posts = useLoaderData()
     return (
       <>
         {loading && <Loader />}
         {!loading && postList.length === 0 && <WelcomMessage />}
-        {!loading &&
-          postList.map((post) => <Post key={post.id} post={post}></Post>)}
+        {!loading && postList.map((post) => <Post key={post.id} post={post}></Post>)}
+        {!loading && posts.map((post) => <Post key={post.id} post={post}></Post>)}
       </>
     );
 };
