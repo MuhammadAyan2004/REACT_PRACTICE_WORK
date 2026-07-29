6c40ab6c88f88a01bc8afc73d7bac945420fee40
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-30 01:42:05 +0500

    Update App.jsx

--- class_8_redux/counter_react_redux/src/App.jsx
+++ class_8_redux/counter_react_redux/src/App.jsx
@@ -2,15 +2,18 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Heading from "./components/heading";
 import DisplayValue from "./components/displayValue";
+import PrivacyToogle from "./components/privacyToogle";
 import Controls from "./components/controls";
+import { useSelector } from "react-redux";
 
 function App() {
+ const privacy = useSelector((store) => store.privacy);
   return (
     <div className="bg-dark text-secondary px-4 py-5 ">
       <div className="py-5 ">
         <Heading />
         <div className="col-lg-6 mx-auto">
-          <DisplayValue />
+          {privacy ? <PrivacyToogle /> : <DisplayValue />}
           <Controls />
         </div>
       </div>
