04e5ce9fffe840f974d7f2bf31bb01aef3130a03
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-30 01:44:49 +0500

    Update displayValue.jsx

--- class_8_redux/counter_react_redux/src/components/displayValue.jsx
+++ class_8_redux/counter_react_redux/src/components/displayValue.jsx
@@ -1,5 +1,8 @@
+import { useSelector } from "react-redux";
+
 const DisplayValue = ()=>{
-    return <p className="fs-5 mb-4 text-center">Counter current value</p>;
+    const counterValue = useSelector((store) => store.counter)
+    return <p className="fs-5 mb-4 text-center">Counter current value: {counterValue}</p>;
 }
 
 export default DisplayValue
