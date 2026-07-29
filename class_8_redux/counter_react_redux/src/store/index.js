957b419d2793b29ab23f9359748797bc91589a6c
Muhammad Ayan <m101ayan92@gmail.com>
2026-07-30 01:45:00 +0500

    Update index.js

--- class_8_redux/counter_react_redux/src/store/index.js
+++ class_8_redux/counter_react_redux/src/store/index.js
@@ -1,7 +1,20 @@
-import {createStore} from "redux"
-
-const counterReducer = (store, action)=>{
-
+import { createStore } from "redux"
+const Initial_Value = {
+    counter: 0,
+    privacy: false
+}
+const counterReducer = (store = Initial_Value, action) => {
+    if (action.type === "Increment") {
+        return { ...store, counter: store.counter + 1 }
+    } else if (action.type === "Decrement") {
+        return { ...store, counter: store.counter - 1 }
+    } else if (action.type === "ADD") {
+        return { ...store, counter: store.counter + Number(action.payload.num) }
+    } else if (action.type === "SUB") {
+        return { ...store, counter: store.counter - Number(action.payload.num) }
+    } else if (action.type === "Privacy") {
+        return { ...store, privacy: !store.privacy }
+    }
     return store
 }
 
