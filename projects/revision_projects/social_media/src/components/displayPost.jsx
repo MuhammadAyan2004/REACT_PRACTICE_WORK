420d38b864c3a9466835ccde9b7da021658d49c0
Muhammad Ayan <m101ayan92@gmail.com>
2026-08-05 05:36:54 +0500

    Create displayPost.jsx

---
+++ projects/revision_projects/social_media/src/components/displayPost.jsx
@@ -0,0 +1,41 @@
+import { useContext } from "react";
+import { FaRegHeart } from "react-icons/fa";
+import { PostProvider } from "../store/contextProvider";
+
+function DisplayPost({ post }) {
+  const {handleLike} = useContext(PostProvider)
+  return (
+    <>
+      <div className="w-full max-w-3xl bg-white rounded-2xl shadow-lg overflow-hidden hover:shadow-2xl transition duration-300">
+        <div className="p-6">
+          <h5 className="text-2xl font-bold text-gray-800 mb-3">
+            {post.userId}
+          </h5>
+          <p className="text-gray-600 leading-relaxed mb-6">{post.title}</p>
+          <p className="text-gray-600 leading-relaxed mb-6">{post.body}</p>
+          <ul className="w-full flex gap-1 flex-wrap ">
+            {post.tags.map((tag, index) => (
+              <li
+                key={index}
+                className="bg-blue-600 text-white px-5 py-2 rounded-lg hover:bg-blue-700 transition duration-300"
+              >
+                {tag}
+              </li>
+            ))}
+          </ul>
+          <ul className="w-full mt-2 flex items-center justify-between text-gray-500">
+            <li
+              className="flex items-center gap-1 cursor-pointer"
+              onClick={() => handleLike(post.id)}
+            >
+              <FaRegHeart /> {post.reactions.likes}
+            </li>
+            <li>views . 405</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default DisplayPost;
