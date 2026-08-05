27c577bfc361047ec219b60a4baa1717e5d3314f
Muhammad Ayan <m101ayan92@gmail.com>
2026-08-06 02:01:25 +0500

    Update displayPost.jsx

--- projects/revision_projects/social_media/src/components/displayPost.jsx
+++ projects/revision_projects/social_media/src/components/displayPost.jsx
@@ -1,16 +1,22 @@
 import { useContext } from "react";
 import { FaRegHeart } from "react-icons/fa";
+import { RxCross2 } from "react-icons/rx";
 import { PostProvider } from "../store/contextProvider";
 
 function DisplayPost({ post }) {
-  const {handleLike} = useContext(PostProvider)
+  const { handleLike, handleDelete } = useContext(PostProvider);
   return (
     <>
       <div className="w-full max-w-3xl bg-white rounded-2xl shadow-lg overflow-hidden hover:shadow-2xl transition duration-300">
         <div className="p-6">
-          <h5 className="text-2xl font-bold text-gray-800 mb-3">
-            {post.userId}
-          </h5>
+          <div className="flex justify-between items-center">
+            <h5 className="text-2xl font-bold text-gray-800 mb-3">
+              {post.userId}
+            </h5>
+            <button className="text-2xl" onClick={()=>handleDelete(post.id)}>
+              <RxCross2 />
+            </button>
+          </div>
           <p className="text-gray-600 leading-relaxed mb-6">{post.title}</p>
           <p className="text-gray-600 leading-relaxed mb-6">{post.body}</p>
           <ul className="w-full flex gap-1 flex-wrap ">
