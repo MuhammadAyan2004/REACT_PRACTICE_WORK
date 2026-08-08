d323d42fef98e1c9e86690caf7cc2cc35b4a244d
Muhammad Ayan <m101ayan92@gmail.com>
2026-08-09 02:02:03 +0500

    Delete contextProvider.jsx

--- projects/revision_projects/social_media/src/store/contextProvider.jsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-export const PostProvider = createContext({
-  postList: [],
-  handleAddPost: () => {},
-  handleLike: () => {},
-  handleDelete: () => {},
-});
