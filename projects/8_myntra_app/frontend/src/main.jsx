2c7414df197f6ff1180fae72fcb3a7e548f13fa5
Muhammad Ayan <m101ayan92@gmail.com>
2026-08-16 04:12:42 +0500

    Update main.jsx

--- projects/8_myntra_app/frontend/src/main.jsx
+++ projects/8_myntra_app/frontend/src/main.jsx
@@ -1,9 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import App from "./routes/App.jsx";
+import Home from "./routes/home.jsx";
+import Bag from "./routes/bag.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [
+      { path: "/", Component: Home },
+      { path: "/bag", Component: Bag },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
