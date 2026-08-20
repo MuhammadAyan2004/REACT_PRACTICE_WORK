c089843f2048852940e77c460716ce9193c47113
Muhammad Ayan <m101ayan92@gmail.com>
2026-08-21 00:03:34 +0500

    Update bagItem.jsx

--- projects/8_myntra_app/frontend/src/components/bagItem.jsx
+++ projects/8_myntra_app/frontend/src/components/bagItem.jsx
@@ -1,47 +1,42 @@
-const BagItem = () => {
-  const item = {
-    id: "001",
-    image: "images/1.jpg",
-    company: "Carlton London",
-    item_name: "Rhodium-Plated CZ Floral Studs",
-    original_price: 1045,
-    current_price: 606,
-    discount_percentage: 42,
-    return_period: 14,
-    delivery_date: "10 Oct 2023",
-    rating: {
-      stars: 4.5,
-      count: 1400,
-    },
-  };
+import { useDispatch } from "react-redux";
+import { GiCrossMark } from "react-icons/gi";
+import { bagAction } from "../store/bagSlice";
+
+const BagItem = ({item}) => {
+    const dispatch = useDispatch()
+    const handleRemoveCart = ()=>{
+        dispatch(bagAction.removeBag(item.id))
+    }
   return (
     <>
-      <div class="bag-item-container">
-        <div class="item-left-part">
-          <img class="bag-item-img" src={item.image} />
+      <div className="bag-item-container">
+        <div className="item-left-part">
+          <img className="bag-item-img" src={item.image} />
         </div>
-        <div class="item-right-part">
-          <div class="company">{item.company}</div>
-          <div class="item-name">{item.item_name}</div>
-          <div class="price-container">
-            <span class="current-price">Rs {item.current_price}</span>
-            <span class="original-price">Rs {item.original_price}</span>
-            <span class="discount-percentage">
+        <div className="item-right-part">
+          <div className="company">{item.company}</div>
+          <div className="item-name">{item.item_name}</div>
+          <div className="price-container">
+            <span className="current-price">Rs {item.current_price}</span>
+            <span className="original-price">Rs {item.original_price}</span>
+            <span className="discount-percentage">
               ({item.discount_percentage}% OFF)
             </span>
           </div>
-          <div class="return-period">
-            <span class="return-period-days">{item.return_period} days</span>{" "}
+          <div className="return-period">
+            <span className="return-period-days">
+              {item.return_period} days
+            </span>{" "}
             return available
           </div>
-          <div class="delivery-details">
+          <div className="delivery-details">
             Delivery by
-            <span class="delivery-details-days">{item.delivery_date}</span>
+            <span className="delivery-details-days">{item.delivery_date}</span>
           </div>
         </div>
 
-        <div class="remove-from-cart" onclick="removeFromBag(${item.id})">
-          X
+        <div className="remove-from-cart" onClick={handleRemoveCart}>
+          <GiCrossMark />
         </div>
       </div>
     </>
