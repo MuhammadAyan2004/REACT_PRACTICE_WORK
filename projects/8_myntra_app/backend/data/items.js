0430f10cb8597fe1974bb71e7a22b3a62e82c206
Muhammad Ayan <m101ayan92@gmail.com>
2026-08-21 00:03:27 +0500

    Update items.js

--- projects/8_myntra_app/backend/data/items.js
+++ projects/8_myntra_app/backend/data/items.js
@@ -1,7 +1,10 @@
-const fs = require('fs');
+const path = require('path');
+const fs = require('fs').promises;
+
+const itemsFile = path.join(__dirname, 'items.json');
 
 async function getStoredItems() {
-    const rawFileContent = await fs.readFile('items.json', { encoding: 'utf-8' });
+    const rawFileContent = await fs.readFile(itemsFile, { encoding: 'utf-8' });
     const data = JSON.parse(rawFileContent);
     const storedItems = data.items ?? [];
     return storedItems;
