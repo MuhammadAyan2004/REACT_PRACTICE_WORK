026ad321e71ca7b2017fcea0f977c91b32f14ec6
Muhammad Ayan <m101ayan92@gmail.com>
2026-08-21 00:03:38 +0500

    Update bagSummary.jsx

--- projects/8_myntra_app/frontend/src/components/bagSummary.jsx
+++ projects/8_myntra_app/frontend/src/components/bagSummary.jsx
@@ -1,43 +1,59 @@
-const BagSummary = ()=>{
-    
-    const bagSummary = {
-        totalItem:2,
-        totalMRP:999,
-        totalDiscount:100,
-        finalPayment: 899
-    }
+import { useSelector } from "react-redux";
 
-    return (
-      <div className="bag-summary">
-        <div className="bag-details-container">
-          <div className="price-header">
-            PRICE DETAILS ({bagSummary.totalItem} Items){" "}
-          </div>
-          <div className="price-item">
-            <span className="price-item-tag">Total MRP</span>
-            <span className="price-item-value">₹{bagSummary.totalMRP}</span>
-          </div>
-          <div className="price-item">
-            <span className="price-item-tag">Discount on MRP</span>
-            <span className="price-item-value priceDetail-base-discount">
-              -₹{bagSummary.totalDiscount}
-            </span>
-          </div>
-          <div className="price-item">
-            <span className="price-item-tag">Convenience Fee</span>
-            <span className="price-item-value">₹99</span>
-          </div>
-          <hr />
-          <div className="price-footer">
-            <span className="price-item-tag">Total Amount</span>
-            <span className="price-item-value">₹{bagSummary.finalPayment}</span>
-          </div>
+const BagSummary = () => {
+  const bagItemIds = useSelector((state) => state.bagSlice);
+  const Items = useSelector((state) => state.items);
+  const finalItems = Items.filter((item) => bagItemIds.includes(item.id));
+  let TotalMRP = 0;
+  let TotalDiscount = 0; 
+  const ConvenienceFee = 99; 
+
+  finalItems.forEach((Item)=>{
+    TotalMRP += Item.original_price;
+    TotalDiscount += Item.original_price - Item.current_price;
+  })
+
+  let finalAmount = TotalMRP - TotalDiscount + ConvenienceFee;
+
+
+  const bagSummary = {
+    totalItem: bagItemIds.length,
+    totalMRP: TotalMRP,
+    totalDiscount: TotalDiscount,
+    finalPayment: finalAmount,
+  };
+
+  return (
+    <div className="bag-summary">
+      <div className="bag-details-container">
+        <div className="price-header">
+          PRICE DETAILS ({bagSummary.totalItem} Items){" "}
         </div>
-        <button className="btn-place-order">
-          <div className="css-xjhrni">PLACE ORDER</div>
-        </button>
+        <div className="price-item">
+          <span className="price-item-tag">Total MRP</span>
+          <span className="price-item-value">${bagSummary.totalMRP}</span>
+        </div>
+        <div className="price-item">
+          <span className="price-item-tag">Discount on MRP</span>
+          <span className="price-item-value priceDetail-base-discount">
+            -₹{bagSummary.totalDiscount}
+          </span>
+        </div>
+        <div className="price-item">
+          <span className="price-item-tag">Convenience Fee</span>
+          <span className="price-item-value">${ConvenienceFee}</span>
+        </div>
+        <hr />
+        <div className="price-footer">
+          <span className="price-item-tag">Total Amount</span>
+          <span className="price-item-value">${bagSummary.finalPayment}</span>
+        </div>
       </div>
-    );
-}
+      <button className="btn-place-order">
+        <div className="css-xjhrni">PLACE ORDER</div>
+      </button>
+    </div>
+  );
+};
 
-export default BagSummary
+export default BagSummary;
