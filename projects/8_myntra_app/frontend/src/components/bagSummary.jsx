ba0083cdd1e47bdc782305c862e8456a8d258580
Muhammad Ayan <m101ayan92@gmail.com>
2026-08-25 23:13:29 +0500

    Update bagSummary.jsx

--- projects/8_myntra_app/frontend/src/components/bagSummary.jsx
+++ projects/8_myntra_app/frontend/src/components/bagSummary.jsx
@@ -29,16 +29,19 @@ const BagSummary = () => {
         <div className="price-header">
           PRICE DETAILS ({bagSummary.totalItem} Items){" "}
         </div>
+
         <div className="price-item">
           <span className="price-item-tag">Total MRP</span>
           <span className="price-item-value">${bagSummary.totalMRP}</span>
         </div>
+
         <div className="price-item">
           <span className="price-item-tag">Discount on MRP</span>
           <span className="price-item-value priceDetail-base-discount">
             -₹{bagSummary.totalDiscount}
           </span>
         </div>
+
         <div className="price-item">
           <span className="price-item-tag">Convenience Fee</span>
           <span className="price-item-value">${ConvenienceFee}</span>
@@ -49,9 +52,11 @@ const BagSummary = () => {
           <span className="price-item-value">${bagSummary.finalPayment}</span>
         </div>
       </div>
+
       <button className="btn-place-order">
         <div className="css-xjhrni">PLACE ORDER</div>
       </button>
+      
     </div>
   );
 };
