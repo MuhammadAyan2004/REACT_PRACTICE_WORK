64261a140137f8e383ce8094140f8855f8d96452
Muhammad Ayan <m101ayan92@gmail.com>
2026-08-25 23:14:06 +0500

    Update bagItem.jsx

--- projects/8_myntra_app/frontend/src/components/bagItem.jsx
+++ projects/8_myntra_app/frontend/src/components/bagItem.jsx
@@ -13,9 +13,12 @@ const BagItem = ({item}) => {
         <div className="item-left-part">
           <img className="bag-item-img" src={item.image} />
         </div>
+
         <div className="item-right-part">
+
           <div className="company">{item.company}</div>
           <div className="item-name">{item.item_name}</div>
+
           <div className="price-container">
             <span className="current-price">Rs {item.current_price}</span>
             <span className="original-price">Rs {item.original_price}</span>
@@ -23,21 +26,25 @@ const BagItem = ({item}) => {
               ({item.discount_percentage}% OFF)
             </span>
           </div>
+
           <div className="return-period">
             <span className="return-period-days">
               {item.return_period} days
             </span>{" "}
             return available
           </div>
+
           <div className="delivery-details">
             Delivery by
             <span className="delivery-details-days">{item.delivery_date}</span>
           </div>
+          
         </div>
 
         <div className="remove-from-cart" onClick={handleRemoveCart}>
           <GiCrossMark />
         </div>
+
       </div>
     </>
   );
