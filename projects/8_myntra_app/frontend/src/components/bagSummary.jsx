3762aa17dd5b0cc09ef59b77dd093fff5d90430f
Muhammad Ayan <m101ayan92@gmail.com>
2026-08-30 23:37:21 +0500

    Update bagSummary.jsx

--- projects/8_myntra_app/frontend/src/components/bagSummary.jsx
+++ projects/8_myntra_app/frontend/src/components/bagSummary.jsx
@@ -1,22 +1,22 @@
 import { useSelector } from "react-redux";
 
 const BagSummary = () => {
+
   const bagItemIds = useSelector((state) => state.bagSlice);
   const Items = useSelector((state) => state.items);
   const finalItems = Items.filter((item) => bagItemIds.includes(item.id));
+
   let TotalMRP = 0;
   let TotalDiscount = 0; 
   const ConvenienceFee = 99; 
 
   finalItems.forEach((Item)=>{
-
     TotalMRP += Item.original_price;
     TotalDiscount += Item.original_price - Item.current_price;
 
   })
 
   let finalAmount = TotalMRP - TotalDiscount + ConvenienceFee;
-
 
   const bagSummary = {
     totalItem: bagItemIds.length,
@@ -29,7 +29,6 @@ const BagSummary = () => {
     <div className="bag-summary">
 
       <div className="bag-details-container">
-
         <div className="price-header">
           PRICE DETAILS ({bagSummary.totalItem} Items){" "}
         </div>
@@ -56,9 +55,11 @@ const BagSummary = () => {
           <span className="price-item-value">${bagSummary.finalPayment}</span>
         </div>
       </div>
+
       <button className="btn-place-order">
         <div className="css-xjhrni">PLACE ORDER</div>
       </button>
+      
     </div>
   );
 };
